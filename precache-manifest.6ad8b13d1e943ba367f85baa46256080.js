20d52dbd750fdf061d9e526524b8981f6600761b
Cam Skubik-Peplaski <monsterboy2@icloud.com>
2020-07-16 19:12:39 -0400

    Updates

---
+++ precache-manifest.6ad8b13d1e943ba367f85baa46256080.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02db1246232bcf021b1f9db14e34300f",
+    "url": "/pf2-websheet/index.html"
+  },
+  {
+    "revision": "7b21fff5a243daf8fe86",
+    "url": "/pf2-websheet/static/css/2.ca6254c2.chunk.css"
+  },
+  {
+    "revision": "c19adc49b1cc43ad9b1a",
+    "url": "/pf2-websheet/static/css/main.e507121e.chunk.css"
+  },
+  {
+    "revision": "7b21fff5a243daf8fe86",
+    "url": "/pf2-websheet/static/js/2.c429ac50.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/pf2-websheet/static/js/2.c429ac50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c19adc49b1cc43ad9b1a",
+    "url": "/pf2-websheet/static/js/main.b01056ac.chunk.js"
+  },
+  {
+    "revision": "4381acb907501fe5d08c",
+    "url": "/pf2-websheet/static/js/runtime-main.bd3b3483.js"
+  }
+]);

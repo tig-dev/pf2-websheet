4c72ee974fab5fae91cca762a62fcdb779a00945
Cam Skubik-Peplaski <monsterboy2@icloud.com>
2020-11-07 22:27:16 -0500

    Updates

---
+++ precache-manifest.a426924d796cc977a95444a8030b1c20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda7e29402d65f465a256cb35d4b433d",
+    "url": "/pf2-websheet/index.html"
+  },
+  {
+    "revision": "2f9d50828fb8e0eae8db",
+    "url": "/pf2-websheet/static/css/2.725ca0dd.chunk.css"
+  },
+  {
+    "revision": "26dcad944c57791e2f01",
+    "url": "/pf2-websheet/static/css/main.f0bf0d30.chunk.css"
+  },
+  {
+    "revision": "2f9d50828fb8e0eae8db",
+    "url": "/pf2-websheet/static/js/2.85ed8809.chunk.js"
+  },
+  {
+    "revision": "ce15a0ffcfe8b91e4f219c1d7bb8c5ef",
+    "url": "/pf2-websheet/static/js/2.85ed8809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26dcad944c57791e2f01",
+    "url": "/pf2-websheet/static/js/main.b2fb2467.chunk.js"
+  },
+  {
+    "revision": "9bbcfbae8beb6414c399",
+    "url": "/pf2-websheet/static/js/runtime-main.e51ee0a6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pf2-websheet/precache-manifest.e36b3f3078546aaa3249b81c549b6dbd.js"
+  "/pf2-websheet/precache-manifest.a426924d796cc977a95444a8030b1c20.js"
 );
 
 self.addEventListener('message', (event) => {
